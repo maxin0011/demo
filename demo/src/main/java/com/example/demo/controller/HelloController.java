03257fe4fee57841102c9697a8d06dc50b2b8703
maxin0011 <maxin0011@163.com>
2020-01-02 19:51:38 +0800

    Update HelloController.java

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+	// 注释下
 	@RequestMapping("hello")
 	public String hello() {
 		return "hello";
